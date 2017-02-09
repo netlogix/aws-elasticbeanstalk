0fd1e6f9ee276a248cc35c46ff5909f1ec66825d
Lienhart Woitok <lienhart.woitok@netlogix.de>
2017-02-09 16:31:50 +0100

    [TASK] Initial code import

---
+++ Scripts/eb-post-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+[ -f /opt/elasticbeanstalk/support/envvars ] || { echo "Fatal. Not on elastic beanstalk." >&2 ; exit 1 ; }
+
+. /opt/elasticbeanstalk/support/envvars
+
+export FLOW_CONTEXT=Production
+
+./flow flow:cache:warmup
+./flow resource:publish --collection static
+./flow doctrine:migrate
---
+++ Scripts/package.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DIST_NAME="${DIST_NAME:-$(basename $(command cd "$(dirname "${BASH_SOURCE[0]}")" && pwd | sed -E 's~(^|/)Packages/Application/Netlogix.Aws.ElasticBeanstalk/Scripts~~'))}"
+VERSION="${VERSION:-$(date +%Y%m%d%H%M%S)}"
+ZIP="${DIST_NAME}-${VERSION}.zip"
+
+echo "Packaging ${DIST_NAME}..."
+
+zip -r "${ZIP}" .ebextensions/ Build/ Configuration/ flow Packages/ Web/.htaccess Web/index.php Web/_Resources/.htaccess composer.json composer.lock -x '*.git*' '*node_modules*'
+
+echo "Created ${ZIP}"
