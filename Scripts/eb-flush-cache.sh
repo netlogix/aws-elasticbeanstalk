070063b2240af197b3b9a09e0342ed41fc74168b
Lienhart Woitok <lienhart.woitok@netlogix.de>
2017-03-22 10:45:20 +0100

    [TASK] Add hook to clear cache after configuration change

---
+++ Scripts/eb-flush-cache.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+[ -f /opt/elasticbeanstalk/support/envvars ] || { echo "Fatal. Not on elastic beanstalk." >&2 ; exit 1 ; }
+
+. /opt/elasticbeanstalk/support/envvars
+
+export FLOW_CONTEXT=Production
+
+./flow setup:cache
+./flow flow:cache:flush
+./flow flow:cache:warmup
