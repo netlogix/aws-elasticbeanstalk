65c00d6ee68ffb01247caac81982e100cdc8e128
Lienhart Woitok <lienhart.woitok@netlogix.de>
2017-06-14 17:48:12 +0200

    [TASK] Store deployment version in flow

---
+++ Scripts/cache-application-version.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Netlogix.Aws.ElasticBeanstalk package.
+ */
+
+const MANIFEST_PATH = '/opt/elasticbeanstalk/deploy/manifest';
+const CACHE_PATH = 'Data/Temporary/Production/Version.txt';
+
+if (file_exists(MANIFEST_PATH)) {
+	$manifestJson = file_get_contents(MANIFEST_PATH);
+	if ($manifestJson !== false) {
+		$manifest = json_decode($manifestJson, true);
+		if ($manifest !== false) {
+			if (array_key_exists('VersionLabel', $manifest)) {
+				$versionLabel = $manifest['VersionLabel'];
+				file_put_contents(CACHE_PATH, $versionLabel);
+			}
+		}
+	}
+}
