df1ce5dae6b93d2b8cc5913b01be1f206da9327d
Lars Lauger <lars.lauger@netlogix.de>
2017-07-27 09:30:02 +0200

    [TASK] Accept jobqueue jobs in WorkerController

--- Classes/Controller/WorkerController.php
+++ Classes/Controller/WorkerController.php
@@ -6,9 +6,12 @@ namespace Netlogix\Aws\ElasticBeanstalk\Controller;
  * This file is part of the Netlogix.Aws.ElasticBeanstalk package.
  */
 
+use Flowpack\JobQueue\Common\Job\JobInterface;
+use Flowpack\JobQueue\Common\Queue\Message;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Core\Booting\Scripts;
 use TYPO3\Flow\Mvc\Controller\ActionController;
+use TYPO3\Flow\Utility\Algorithms;
 
 class WorkerController extends ActionController
 {
@@ -22,18 +25,34 @@ class WorkerController extends ActionController
 	protected $flowSettings;
 
 	/**
-	 * Executes the given flow command
+	 * Executes the given job
 	 *
-	 * @param string $command
-	 * @param array $arguments
+	 * @param string $payload The message payload
+	 * @param string $queue Queue used to execute the job on. For most jobs the queue implementation is irrelevant.
 	 * @return string
 	 * @throws \Exception
 	 */
-	public function commandAction($command, array $arguments = []) {
-		if (Scripts::executeCommand($command, $this->flowSettings, true, $arguments)) {
-			return '';
+	public function commandAction($payload, $queue = 'awseb-fakequeue')
+	{
+		$job = unserialize($payload);
+		if ($job instanceof JobInterface) {
+			$receiveCount = 0;
+			if ($this->request->getHttpRequest()->hasHeader('X-aws-sqsd-receive-count')) {
+				$receiveCount = $this->request->getHttpRequest()->getHeaders()->get('X-aws-sqsd-receive-count');
+			}
+			$message = new Message(Algorithms::generateUUID(), serialize($job), $receiveCount);
+			$result = Scripts::executeCommand('flowpack.jobqueue.common:job:execute', $this->flowSettings, true, [
+				$queue,
+				base64_encode(serialize($message))
+			]);
+			if ($result) {
+				return '';
+			} else {
+				throw new \Exception('Job execution failed', 1500368045);
+			}
 		} else {
-			throw new \Exception('Command execution failed', 1500368045);
+			throw new \Exception('Not a valid Job', 1501078167);
 		}
 	}
+
 }
